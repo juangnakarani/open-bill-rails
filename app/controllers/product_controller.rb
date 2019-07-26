8bed126c4169098fcc8631e0cfe04f9705e1df8c
juangnakarani <juang.nakarani@gmail.com>
2019-07-26 09:55:39 +0700

    implemented will_paginate

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,6 @@
 class ProductController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.paginate(page: params[:page])
+
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+    self.per_page = 25
 end
